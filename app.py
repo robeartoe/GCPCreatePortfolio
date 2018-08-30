d2193f940ccd1dbb94d06e68a5302667be3117d1
Robert Ruiz <rsblooxpert@gmail.com>
2018-08-30 15:27:22 -0700

    Finished general project, need to detail with info.

--- app.py
+++ app.py
@@ -3,8 +3,16 @@ from flask import Flask,request,render_template,url_for,redirect
 
 app = Flask(__name__)
 
-app.config['DEBUG'] = os.environ['DEBUG']
+app.config['DEBUG'] = os.environ['FLASK_DEBUG']
 
 @app.route("/")
 def main():
     return render_template("main.html")
+    
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
